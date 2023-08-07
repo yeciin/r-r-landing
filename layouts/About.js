b3b7ca1beff225a7066c7f0d1dc118b636919962
Hadi <h.hassan@fleetrunnr.com>
2023-08-07 18:31:16 +0300

    Added images to about page

--- layouts/About.js
+++ layouts/About.js
@@ -1,16 +1,29 @@
 import { markdownify } from "@lib/utils/textConverter";
+import Image from "next/image";
 
 function About({ data }) {
   const { frontmatter } = data;
-  const { title, info, abouts } = frontmatter;
+  const { title, info, banner, logo } = frontmatter;
   return (
     <section className="section">
       <div className="container">
+        <Image
+          src={banner.image}
+          alt="workflow image"
+          width={1920}
+          height={296}
+        />
+        <br></br>
+        <Image
+          src={logo.image}
+          alt="workflow image"
+          width={1920}
+          height={296}
+        />
+        <br></br>
         {markdownify(title, "h1", "text-center font-normal")}
         {markdownify(info, "p", "text-center mt-6 mb-6")} {}
-        <div className="section row -mt-6">
-          {}
-        </div>
+        <div className="section row -mt-6">{}</div>
       </div>
     </section>
   );
